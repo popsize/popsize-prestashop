9a2473ab1d9f6b9ba8d5c6bc9d7d48b9c9d2f4de
Nicolas Micaux <nicolas@popsize.ai>
2025-06-11 09:41:30 +0000

    cs fixer

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate');
@@ -6,4 +7,3 @@ header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
 header('Location: ../');
 exit;
-
